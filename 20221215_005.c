e862ab13f74259992623bbd533ec3d5405968896
ArthurMLins <119690131+ArthurMLins@users.noreply.github.com>
2022-12-20 15:54:05 -0300

    Create 20221215_005.c

---
+++ 20221215_005.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,area;
+
+    printf("Digite o lado do quadrado: ");
+    scanf("%d", &a);
+
+
+
+    area = a*a*2;
+
+    printf("\n""o dobro da area do quadrado é: ""%d",area);
+    return 0;
+
+}
