0e879aeb5b636b49fe4781dc5e1d15a332fdfaa8
ArthurMLins <119690131+ArthurMLins@users.noreply.github.com>
2022-12-20 15:53:07 -0300

    Create 20221215_003.c

---
+++ 20221215_003.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,conversao;
+
+    printf("Digite o tamanho em metros: ");
+    scanf("%d", &a);
+
+
+
+    conversao = a*100;
+
+    printf("\n""A sua medida em cm: ""%d",conversao);
+    return 0;
+
+}
