74f7ee813e81af9cd9aeaa3e16b90733c6da54e6
ArthurMLins <119690131+ArthurMLins@users.noreply.github.com>
2022-12-20 15:53:31 -0300

    Create 20221215_004.c

---
+++ 20221215_004.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,area;
+
+    printf("Digite o raio: ");
+    scanf("%d", &a);
+
+
+
+    area = a*a*3.14;
+
+    printf("\n""O raio do circulo e: ""%d",area);
+    return 0;
+
+}
